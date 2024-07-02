613fad410390847b0d8d91e306b13cd534c1134d
Sweet-Pillow <daniel_h.m@outlook.com>
2024-07-01 21:18:08 -0300

    Componentization Field

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,5 +1,7 @@
+<script setup>
+	import Field from '../../components/Field/index.vue'
+</script>
+
 <template>
-	<div class=" h-4/6 w-[26%] p-5">
-		<div class="h-full w-full bg-cover bg-[url(https://cartola.globo.com/dist/11.18.0/img/campinho-sem-borda.svg)]"></div>
-	</div>
+	<Field></Field>
 </template>
