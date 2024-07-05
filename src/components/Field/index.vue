9bb88de956a5ddf1c3ee72b10b829b6c9a9b4bdc
Sweet-Pillow <daniel_h.m@outlook.com>
2024-07-05 12:31:52 -0300

    Header componentize

--- src/components/Field/index.vue
+++ src/components/Field/index.vue
@@ -1,7 +1,7 @@
 <template>
 	<!-- <div class=" h-4/6 w-[26%] p-5"> -->
 	<!-- <div class=" h-[72%] w-[30%] p-5"> -->
-	<div class=" h-[430px] w-[450px] p-5">
+	<div class=" h-[435px] w-[450px] p-5">
 		<div class="h-full w-full bg-cover bg-[url(https://cartola.globo.com/dist/11.18.0/img/campinho-sem-borda.svg)]">
 		</div>
 	</div>
---
+++ src/components/Header/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<nav class="bg-orange-500 h-1/6 p-5 flex items-center justify-end">
+		<button class="w-max h-max border-4 border-transparent active:rounded active:border-white">
+			<svg xmlns="http://www.w3.org/2000/svg" height="80px" width="80px" viewBox="0 -960 960 960" fill="#FFEEDC">
+				<path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+			</svg>
+		</button>
+	</nav>
+</template>
