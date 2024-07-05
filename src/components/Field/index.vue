867800837341d37dc46c7608615bdcb48c35277d
Sweet-Pillow <daniel_h.m@outlook.com>
2024-07-05 16:48:49 -0300

    Player Select Prototype

--- src/components/Field/index.vue
+++ src/components/Field/index.vue
@@ -1,8 +1,67 @@
+<script setup>
+	let position = ["ATA", "MEI", "LAT", "ZAG", "GOL", "TEC"]
+</script>
+
 <template>
 	<!-- <div class=" h-4/6 w-[26%] p-5"> -->
 	<!-- <div class=" h-[72%] w-[30%] p-5"> -->
-	<div class=" h-[435px] w-[450px] p-5">
-		<div class="h-full w-full bg-cover bg-[url(https://cartola.globo.com/dist/11.18.0/img/campinho-sem-borda.svg)]">
+	<div class="w-[550px] h-[570px] p-5">
+		<div class="relative h-full w-full bg-no-repeat bg-center bg-cover bg-[url(https://cartola.globo.com/dist/11.18.0/img/campinho-sem-borda.svg)]">
+			<div class="transition-transform hover:scale-125 absolute top-12 left-36 flex justify-center items-center flex-col">
+				<div class="h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[0] }}</h3>
+			</div>
+			<div class="transition-transform hover:scale-125 absolute top-12 right-36 flex justify-center items-center flex-col">
+				<div class=" h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[0] }}</h3>
+			</div>
+			<div class="transition-transform hover:scale-125 absolute top-44 left-20 flex justify-center items-center flex-col">
+				<div class=" h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[1] }}</h3>
+			</div>
+			<div class="transition-transform hover:scale-125 absolute top-44 left-44 flex justify-center items-center flex-col">
+				<div class=" h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[1] }}</h3>
+			</div>
+			<div class="transition-transform hover:scale-125 absolute top-44 right-20 flex justify-center items-center flex-col">
+				<div class=" h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[1] }}</h3>
+			</div>
+			<div class="transition-transform hover:scale-125 absolute top-44 right-44 flex justify-center items-center flex-col">
+				<div class=" h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[1] }}</h3>
+			</div>
+			<div class="transition-transform hover:scale-125 absolute bottom-40 left-14 flex justify-center items-center flex-col">
+				<div class=" h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[2] }}</h3>
+			</div>
+			<div class="transition-transform hover:scale-125 absolute bottom-40 left-40 flex justify-center items-center flex-col">
+				<div class=" h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[3] }}</h3>
+			</div>
+			<div class="transition-transform hover:scale-125 absolute bottom-40 right-14 flex justify-center items-center flex-col">
+				<div class=" h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[2] }}</h3>
+			</div>
+			<div class="transition-transform hover:scale-125 absolute bottom-40 right-40 flex justify-center items-center flex-col">
+				<div class=" h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[3] }}</h3>
+			</div>
+			<div class="transition-transform hover:scale-125 absolute inset-x-0 bottom-12 flex justify-center items-center flex-col">
+				<div class=" h-10 w-10 border border-black rounded-full">
+				</div>
+				<h3 class="select-none">{{ position[4] }}</h3>
+			</div>
 		</div>
 	</div>
 </template>
--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,7 +1,7 @@
 <template>
-	<nav class="bg-orange-500 h-1/6 p-5 flex items-center justify-end">
+	<nav class="bg-orange-500 h-16 p-5 flex items-center justify-end">
 		<button class="w-max h-max border-4 border-transparent active:rounded active:border-white">
-			<svg xmlns="http://www.w3.org/2000/svg" height="80px" width="80px" viewBox="0 -960 960 960" fill="#FFEEDC">
+			<svg xmlns="http://www.w3.org/2000/svg" height="50px" width="50px" viewBox="0 -960 960 960" fill="#FFEEDC">
 				<path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
 			</svg>
 		</button>
